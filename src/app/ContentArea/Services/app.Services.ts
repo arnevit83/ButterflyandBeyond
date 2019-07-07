2b03d6d9291959f5426b9b909217a66fb41539c5
duncan <arnevit@msn.com>
2019-07-07 13:33:54 +0100

    Text Amends

--- src/app/ContentArea/Services/app.Services.ts
+++ src/app/ContentArea/Services/app.Services.ts
@@ -11,8 +11,8 @@ export class ServicesComponent {
 
 
   public constructor(private titleService: Title, private meta: Meta) {
-       this.titleService.setTitle( "" );
-       this.meta.updateTag({ name: 'description', content: '' });
-       this.meta.updateTag({ name: 'keywords', content: '' });
+       this.titleService.setTitle( "Weekend retreats, yoga classes & online coaching for childless women" );
+       this.meta.updateTag({ name: 'description', content: 'Personal coach and yoga teacher Sophia Andeh offers childless women online coaching, online yoga classes, private yoga classes in Oxford, and nurturing weekend yoga retreats at the Abbey, Sutton Courtenay, Oxford. Work with Sophia to recover from the grief of childlessness and rediscover a sense of joy and purpose.' });
+       this.meta.updateTag({ name: 'keywords', content: 'Online coaching,Online yoga classes,One to one yoga classes,Online coaching,Yoga oxford,Yoga classes Oxford,Childless women,Weekend yoga retreats,Weekend retreat,Private yoga classes' });
   }
 }
--- src/app/ContentArea/app.Home.ts
+++ src/app/ContentArea/app.Home.ts
@@ -19,8 +19,8 @@ export class HomeComponent {
   faCalendarAlt = faCalendarAlt;
 
   public constructor(private titleService: Title, private meta: Meta) {
-       this.titleService.setTitle( "" );
-       this.meta.updateTag({ name: 'description', content: '' });
-       this.meta.updateTag({ name: 'keywords', content: '' });
+       this.titleService.setTitle( "Need help on how to cope with being childless not by choice?" );
+       this.meta.updateTag({ name: 'description', content: 'Personal coach and yoga teacher Sophia Andeh offers weekend yoga retreats, online coaching, and private yoga classes in Oxford for women childless not by choice. Work with Sophia to regain your confidence and motivation, explore new perspectives and create a rewarding life, living with optimism and joy. ' });
+       this.meta.updateTag({ name: 'keywords', content: 'Yoga teacher, Meditation teacher,Online coaching,Life unexpected,Childless women,How to cope with childless not by choice,Being childless not by choice,Weekend retreat,Weekend yoga retreats,Online yoga classes,Private yoga classes,Yoga classes Oxford' });
   }
 }
